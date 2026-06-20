12c3036c2bc1efcb590188e15dcd5861b1643934
kesh-ag <keshav.agrawal@machinelearningtech.ai>
2026-06-20 13:33:11 +0530

    Bug fix

--- task_management/database.py
+++ task_management/database.py
@@ -1,9 +1,10 @@
 from sqlmodel import Session, create_engine
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
-engine = create_engine(Database_URL) 
+engine = create_engine(DATABASE_URL) 
 
 def get_session():
     with Session(engine) as session:
